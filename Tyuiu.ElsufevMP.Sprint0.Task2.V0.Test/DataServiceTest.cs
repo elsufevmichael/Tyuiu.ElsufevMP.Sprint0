029f3a8f7afff19cfb48501879d155450068a037
Tyuiu.ElsufevMP@outlook.com <mihal.els.32@gmail.com>
2023-09-05 18:45:13 +0500

    Task2

---
+++ Tyuiu.ElsufevMP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ElsufevMP.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ElsufevMP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Michael";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Hello, Michael", res);
+        }
+    }
+}
