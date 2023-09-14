b326e6bd9b2d8c4785aacd060fd18ed247fd9f7f
Tyuiu.ElsufevMP@outlook.com <mihal.els.32@gmail.com>
2023-09-14 15:18:41 +0500

    Task6

---
+++ Tyuiu.ElsufevMP.Sprint0.Task6.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ElsufevMP.Sprint0.Task6.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tyuiu.ElsufevMP.Sprint0.Task6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElsufevMP.Sprint0.Task6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ElsufevMP.Sprint0.Task6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46404f25-d06c-4985-9043-9a45570ffc58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
